3c1bf9d5443b6545e180f894e903c026e456df51
Oseec <113547766+Oseec@users.noreply.github.com>
2023-10-26 09:51:16 -0600

    Inicio

---
+++ Tarea3BDI/Tarea3BDI/Models/EmpleadoModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlTypes;
+
+namespace Tarea3BDI.Models
+{
+    public class EmpleadoModel
+    {
+
+        public int Id { get; set; }
+
+        public string NombreEmpleado { get; set; }
+
+        public DateOnly FechaDeNacimiento { get; set; }
+
+        public int IdTipoDocumento { get; set; }
+
+        public string ValorTipoDocumento { get; set; }
+
+        public int IdDepartamento { get; set; }
+        
+        public int IdPuesto { get; set; }
+
+        public string Usuario { get; set; }
+
+        public string Password { get; set; }
+
+        public SqlBoolean EsActivo { get; set; }
+
+    }
+}
---
+++ Tarea3BDI/Tarea3BDI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea3BDI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
